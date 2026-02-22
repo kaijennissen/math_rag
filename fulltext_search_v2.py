41cf5b0c7606604c8c8bea02c7aa90386f230d52
Kai Jennissen <kai@jennissen.org>
2026-02-22 12:58:05 +0100

    first draft of dataclass version

---
+++ fulltext_search_v2.py
@@ -0,0 +1,61 @@
+"""Main takeaway from v1 is, that calculating bm25 required a lot of
+informations (number of documents, average length of the documents, etc.)
+ to be calculated repeatedly.
+All these informations could also be precalculated and stored alongside the
+inverted index. While this could be done by further nesting dictionaries, I think
+a more object-oriented approach (using lightweight dataclasses) is more convincing.
+"""
+
+from collections import Counter
+from dataclasses import dataclass, field
+
+
+@dataclass
+class Document:
+    content: str
+    terms: list[str] = field(init=False)
+    id: int = field(init=False)
+    length: int = field(init=False)
+    term_count: Counter = field(init=False)
+
+    def __post_init__(self):
+        self.terms = self.content.split()
+        self.id = hash(self.content)
+        self.length = len(self.terms)  # derive from term_count
+        self.term_count = Counter(self.terms)
+
+
+@dataclass
+class InvertedIndexEntry:
+    """Class for holding a single entry in the Inverted Index"""
+
+    doc_id: int
+    count: int
+
+
+@dataclass
+class InvertedIndex:
+    """Inverted Index class that stores additional meta-informations."""
+
+    number_of_documents: int = 0
+    average_document_length: float = 0
+    entries: dict[str, InvertedIndexEntry] = field(default_factory=dict)
+
+    def add_document(self, doc: Document):
+        self.average_document_length = (
+            self.average_document_length * self.number_of_documents + doc.length
+        ) / (self.number_of_documents + 1)
+        self.number_of_documents += 1
+        self.entries  # missing
+
+    def add_documents(self, docs: list[Document]):
+        for doc in docs:
+            self.add_document(doc)
+
+
+doc = Document("the cat sat on the mat")
+inv_index = InvertedIndex()
+inv_index.add_document(doc)
+doc_list = [Document("the cat sat on the mat"), Document("the dog sat on the log")]
+inv_index.add_documents(doc_list)
+inv_index
